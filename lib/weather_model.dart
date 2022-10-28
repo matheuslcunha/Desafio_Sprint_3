026aca0bc7a94dd8376b88263da5bb79701a5f75
Matheus Cunha <teteucunha@hotmail.com>
2022-10-28 14:06:45 -0300

    Criando model para Weather (previsão do clima no dia da busca)

---
+++ lib/weather_model.dart
@@ -0,0 +1,28 @@
+import 'package:desafio_sprint_3/forecast_model.dart';
+
+class Weather {
+  String? date;
+  String? time;
+  int? temp;
+  String? description;
+  String? city;
+  String? sunrise;
+  String? sunset;
+  String? condition_slug;
+  List<Forecast>? forecast;
+
+  Weather();
+
+  void fromData(buff) async {
+    final data = await buff;
+    date = data['date'];
+    time = data['time'];
+    temp = data['temp'];
+    description = data['description'];
+    city = data['city'];
+    sunrise = data['sunrise'];
+    sunset = data['sunset'];
+    condition_slug = data['condition_slug'];
+    forecast = data['forecast'];
+  }
+}
