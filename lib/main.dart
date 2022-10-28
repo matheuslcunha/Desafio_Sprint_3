354ffb1c241d13a31aa2115008caac3a53978e49
Matheus Cunha <teteucunha@hotmail.com>
2022-10-28 14:02:08 -0300

    Finalizando o arquivo main

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:desafio_sprint_3/screen1.dart';
+import 'package:desafio_sprint_3/search_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Container(),
+      home: searchScreen(),
     );
   }
 }
